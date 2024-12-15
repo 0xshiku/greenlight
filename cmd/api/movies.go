45913aa3ac3e78d5ac1ddda0ddc2474995f1edd1
0xshiku <olachico@icloud.com>
2024-12-15 19:18:11 +0000

    feat: sending error messages

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -40,7 +40,6 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	// Create an envelope{"movie": movie} instance and pass it to writeJSON(), instead of passing the plain movie struct
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
-		app.logger.Println(err)
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 }
