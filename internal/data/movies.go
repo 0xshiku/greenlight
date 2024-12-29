ab3bc6ed1b84fe6c8becf3b113cf6bc8841e6cad
0xshiku <olachico@icloud.com>
2024-12-29 23:33:00 +0000

    feat: sorting list

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"github.com/lib/pq"
 	"greenlight/internal/validator"
 	"time"
@@ -205,13 +206,15 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
 	// Construct the SQL query to retrieve all movie records.
 	// Update the SQL query to include the filter conditions
 	// Use full-text search for the title filter
-	query := `
+	// Add an ORDER BY clause and interpolate the sort column and direction.
+	// Importantly notice that we also include a secondary sort on the movie ID to ensure a consistent ordering.
+	query := fmt.Sprintf(`
 		SELECT id, created_at, title, year, runtime, genres, version 
 		FROM movies 
 		WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '') 
 		AND (genres @> $2 OR $2 = '{}') 
-		ORDER BY id
-	`
+		ORDER BY %s %s, id ASC
+	`, filters.sortColumn(), filters.sortDirection())
 
 	// Create a context with a 3-second timeout.
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
